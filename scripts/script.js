77a7fc8e901c0e89569f67275c630077ea813e31
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-08-14 10:50:04 -0300

    input values onclick

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 var inputTitleElement = document.querySelector('.header-content #title-input');
-console.log(inputTitleElement);
+// console.log(inputTitleElement.value);
 var inputSubjectElement = document.querySelector(
   '.header-content #subject-input'
 );
-console.log(inputSubjectElement);
+// console.log(inputSubjectElement.value);
+
+var buttonElement = document.querySelector('.header-content .btn-add');
+
+// console.log(buttonElement);
+
+function todoPrint() {
+  console.log(inputTitleElement.value);
+  console.log(inputSubjectElement.value);
+}
+
+buttonElement.onclick = todoPrint;
