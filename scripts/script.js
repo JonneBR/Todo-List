8860e4108baeada1d34743c59d174b77d39211e2
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-08-14 10:34:39 -0300

    script file and querySelectors

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+var inputTitleElement = document.querySelector('.header-content #title-input');
+console.log(inputTitleElement);
+var inputSubjectElement = document.querySelector(
+  '.header-content #subject-input'
+);
+console.log(inputSubjectElement);
