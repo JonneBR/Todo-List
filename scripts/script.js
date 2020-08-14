17e3aced7c724aef9ecf9b4d8650ce0a26dccdbc
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-08-14 13:59:34 -0300

    showTodos

--- scripts/script.js
+++ scripts/script.js
@@ -9,19 +9,28 @@ var position = '';
 
 function addTodos(todos, listTodos) {
   listTodos.push(todos);
-  //   console.log('?', teste);
+  console.log('?', listTodos);
 }
 
 function showTodos(list, divId) {
-  var auxHtml = '<strong>' + '<p>';
+  //   console.log('WHERE:', list[0].title);
+  var teste = document.getElementById(divId);
+  console.log('tt', teste);
+  var auxHtml;
 
   for (var i = 0; i < list.length; i++) {
-    auxHtml +=
-      '<strong>' + '<p>' + list[i].title + '<strong>' + '<p>' + list[i].subject;
-    '<strong>' + '<p>';
+    console.log('title', list[i]);
+    teste +=
+      '<strong>' +
+      list[i].title +
+      '</strong>' +
+      '<p>' +
+      list[i].subject +
+      '</p>';
+    console.log('auxVALUE:', auxHtml);
   }
-  values = document.getElementsByClassName(divId).innerHTML = auxHtml;
-  console.log('values: ', values);
+  document.getElementById(divId).innerHTML = teste;
+  console.log('values: ', divId);
 }
 
 // console.log(inputSubjectElement.value);
@@ -32,16 +41,17 @@ var buttonElement = document.querySelector('.header-content .btn-add');
 
 function todoPrint() {
   var titleValue = document.querySelector('.header-content #title-input').value;
-  // console.log(inputTitleElement.value);
   var subjectValue = document.querySelector('.header-content #subject-input')
     .value;
+
+  //   var teste = document.getElementById('teste');
+
   //   console.log(inputTitleElement.value);
   //   console.log(inputSubjectElement.value);
   var todos = new todoInformation(titleValue, subjectValue);
 
   console.log('teste: ', todos);
   addTodos(todos, listTodos);
-
   showTodos(listTodos, 'item-container');
 }
 
