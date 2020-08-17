5664e9b4e4d66220a6a0242d4ecd32e03b368423
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-08-17 12:32:10 -0300

    function updateInputs created but not working

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,15 @@ function addTodos(todos, listTodos) {
   console.log('?', listTodos);
 }
 
+function updateInputs(eventClick) {
+  console.log('funcionou');
+  position = eventClick.getAttribute('rel');
+  teste = document.querySelector('#item-container1 strong').textContent;
+  // document.get('cpf').value = listaClientes[posicao].cpf;
+  console.log('funcionou', position.value);
+  console.log('strong', teste);
+}
+
 function clearInputs() {
   var titleValue = (document.querySelector(
     '.header-content #title-input'
@@ -23,8 +32,10 @@ function clearInputs() {
 }
 
 var buttonElement = document.querySelector('.header-content .btn-add');
+console.log(buttonElement);
 
 var room = 0;
+
 function todoPrint() {
   var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
   console.log('todos', todos);
@@ -41,6 +52,7 @@ function todoPrint() {
 
   childDiv.setAttribute('id', 'item-container' + room);
   childDiv.setAttribute('class', 'item-container');
+  // for(var room = 0; room < list)
   childDiv.innerHTML =
     '' +
     '<strong>' +
@@ -49,13 +61,26 @@ function todoPrint() {
     '<p>' +
     subjectValue +
     '</p>' +
-    '<button class="update-todo" rel="' +
+    '<button id="update-todo" rel="' +
+    room +
     '" class="update-todo">UPDATE</button>' +
     '<button class="delete-todo" rel="' +
     '" class="delete-todo">DELETE</button>';
   parentDiv.appendChild(childDiv);
 
   clearInputs();
+  // updateButtonOnClick();
 }
 
-buttonElement.onclick = todoPrint;
+document.onclick = function (event) {
+  eventClick = event.target || event.SrcElement;
+
+  switch (eventClick.id) {
+    case 'btn-add':
+      todoPrint();
+      break;
+    case 'update-todo':
+      updateInputs(eventClick, room);
+  }
+  // buttonElement.onclick = todoPrint;
+};
